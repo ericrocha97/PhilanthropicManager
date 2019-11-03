7bc19c937a350e7cf4b25927c2a1e8ea82fc5ccf
Bruno Henrique Raiher <48607967+bhraiher@users.noreply.github.com>
2019-11-03 18:40:21 -0300

    V1.0.8
    Adicionado a tela de cadastro de TIPOS DE DOCUMENTOS.

---
+++ app/src/main/java/cf/ericrocha/philanthropicmanager/document_types.java
@@ -0,0 +1,14 @@
+package cf.ericrocha.philanthropicmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class document_types extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_document_types);
+    }
+}
